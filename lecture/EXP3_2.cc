99b24ba670094cd9f5046089abc6ecaf204dffc5
tristertian <tristertian@qq.com>
2025-11-28 16:57:54 +0800

    EXP3_repair

--- lecture/EXP3_2.cc
+++ lecture/EXP3_2.cc
@@ -17,8 +17,8 @@ struct monitor{
 };
 
 int select_execute_process(monitor* obj){
-    int max_priority = -1;
-    int selected_index = -1;
+    int max_priority = 0;
+    int selected_index = -1;                            //why not use 114514? because it cause a bug
 
     for (int i=0; i<4; i++) {
         if (obj[i].pState == 1 && obj[i].pCPU > 0) {
@@ -46,24 +46,28 @@ int main(){
                         {"QQ.exe",      103, 5, 1, 4, "tencet QQ"},
                         {"CPPIDE.exe",  104, 3, 1, 2, "CFree 5.0"},}; 
     int cnt=0;    
-    while (!is_allzero(process)){
+    while (is_allzero(process) != true){
         cnt++;
         int execute_index = select_execute_process(process);
-        if (execute_index != -1)
-            process[execute_index].pState = 2;          // 进入执行状态
+        if (execute_index != -1){
+            process[execute_index].pState = 2;      //change state to Excute
+        }
 
         for (int i = 0; i < 4; i++){
-            if (process[i].pState == 1){                // 就绪
-                process[i].pPriority += 1;
-            }
-            else if (process[i].pState == 2){           // 执行
+            if (process[i].pState == 1){
+                process[i].pPriority += 1;          //keep its state
+            }else if (process[i].pState == 2){
                 process[i].pPriority -= 3;
-                process[i].pCPU -= 1;
+                process[i].pCPU -= 1;               //excute
+                process[i].display(cnt); 
+                process[i].pState = 1;              //recover state to Ready
 
-                if (process[i].pCPU == 0)               // 运行结束
-                    process[i].pState = 3;              // 改为停止
+                if (process[i].pCPU == 0){
+                    process[i].pState = 3;
+                }
+                continue;
             }
-            process[i].display(cnt);
+            process[i].display(cnt);                //if no excute, then display here.
         }
         cout << '\n';
     }
