a53af365e5e8cbf232ff878e568324e08d48a631
tristertian <tristertian@qq.com>
2025-12-21 22:16:25 +0800

    EXP4_2

--- lecture/EXP4_2.cc
+++ lecture/EXP4_2.cc
@@ -2,73 +2,59 @@
 #include <string>
 using namespace std;
 
-/* ==========  Teacher  ========== */
+// ***decelaration***
 class Teacher {
 protected:
     string name, age, sex, address, phone;
-    string title;          // 职称
+    string title;
 public:
     Teacher(string n, string a, string s, string t, string addr, string tel);
-    void display() const;  // 输出 Teacher 信息
+    void display() const;
 };
 
-/* ==========  Cadre  ========== */
 class Cadre {
 protected:
     string name, age, sex, address, phone;
-    string post;           // 职务
+    string post;
 public:
     Cadre(string n, string a, string s, string p, string addr, string tel);
 };
 
-/* ==========  Teacher_Cadre  ========== */
 class Teacher_Cadre : public Teacher, public Cadre {
-    double wages;          // 工资
+protected:
+    double wages;
 public:
-    Teacher_Cadre(string n, string a, string s,
-                  string t, string p, string addr, string tel, double w);
-    void show() const;     // 按要求输出全部信息
+    Teacher_Cadre(string n, string a, string s, string t, string p, string addr, string tel, double w);
+    void show() const;
 };
 
-/* ----------------------------------------------------------
- * 类外定义成员函数
- * ---------------------------------------------------------- */
-Teacher::Teacher(string n, string a, string s,
-                 string t, string addr, string tel)
-    : name(std::move(n)), age(std::move(a)), sex(std::move(s)),
-      title(std::move(t)), address(std::move(addr)), phone(std::move(tel)) {}
+// ***defination***
+// class Teacher
+Teacher::Teacher(string n, string a, string s, string t, string addr, string tel)
+    : name(std::move(n)), age(std::move(a)), sex(std::move(s)), title(std::move(t)), address(std::move(addr)), phone(std::move(tel)) {}
 
 void Teacher::display() const {
     cout << name << ' ' << age << ' ' << sex << ' '
          << title << ' ' << address << ' ' << phone;
 }
-
-Cadre::Cadre(string n, string a, string s,
-             string p, string addr, string tel)
-    : name(std::move(n)), age(std::move(a)), sex(std::move(s)),
-      post(std::move(p)), address(std::move(addr)), phone(std::move(tel)) {}
-
-Teacher_Cadre::Teacher_Cadre(string n, string a, string s,
-                             string t, string p, string addr, string tel, double w)
-    : Teacher(n, a, s, t, addr, tel),
-      Cadre(n, a, s, p, addr, tel),
-      wages(w) {}
+// class Cadre
+Cadre::Cadre(string n, string a, string s, string p, string addr, string tel)
+    : name(std::move(n)), age(std::move(a)), sex(std::move(s)), post(std::move(p)), address(std::move(addr)), phone(std::move(tel)) {}
+// class Teacher_Cadre
+Teacher_Cadre::Teacher_Cadre(string n, string a, string s, string t, string p, string addr, string tel, double w)
+    : Teacher(n, a, s, t, addr, tel), Cadre(n, a, s, p, addr, tel), wages(w) {}
 
 void Teacher_Cadre::show() const {
-    Teacher::display();          // 调用 Teacher 的 display
+    Teacher::display();
     cout << ' ' << Cadre::post << ' ' << wages << endl;
 }
 
-/* ----------------------------------------------------------
- * main
- * ---------------------------------------------------------- */
+
 int main() {
     string name, age, sex, title, addr, phone, post;
     double wages;
-    cin >> name >> age >> sex >> title
-        >> addr >> phone >> post >> wages;
+    cin >> name >> age >> sex >> title >> addr >> phone >> post >> wages;
 
-    Teacher_Cadre tc(name, age, sex, title, post, addr, phone, wages);
-    tc.show();
-    return 0;
+    Teacher_Cadre ng(name, age, sex, title, post, addr, phone, wages);
+    ng.show();
 }
