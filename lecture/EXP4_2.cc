ad9a9df8fa926e627c7ba3de3fc445469ecc35d4
Tristertian <tristertian@qq.com>
2025-12-23 02:27:55 +0000

    EXP4_2

--- lecture/EXP4_2.cc
+++ lecture/EXP4_2.cc
@@ -8,7 +8,8 @@ protected:
     string name, age, sex, address, phone;
     string title;
 public:
-    Teacher(string n, string a, string s, string t, string addr, string tel);
+    Teacher(string n, string a, string s, 
+        string t, string addr, string tel);
     void display() const;
 };
 
@@ -17,32 +18,43 @@ protected:
     string name, age, sex, address, phone;
     string post;
 public:
-    Cadre(string n, string a, string s, string p, string addr, string tel);
+    Cadre(string n, string a, string s, 
+        string p, string addr, string tel);
 };
 
 class Teacher_Cadre : public Teacher, public Cadre {
 protected:
     double wages;
 public:
-    Teacher_Cadre(string n, string a, string s, string t, string p, string addr, string tel, double w);
+    Teacher_Cadre(string n, string a, string s, 
+        string t, string p, string addr, 
+        string tel, double w);
     void show() const;
 };
 
 // ***defination***
 // class Teacher
-Teacher::Teacher(string n, string a, string s, string t, string addr, string tel)
-    : name(std::move(n)), age(std::move(a)), sex(std::move(s)), title(std::move(t)), address(std::move(addr)), phone(std::move(tel)) {}
+Teacher::Teacher(string n, string a, string s, 
+    string t, string addr, string tel)
+    : name(std::move(n)), age(std::move(a)), 
+    sex(std::move(s)), title(std::move(t)), 
+    address(std::move(addr)), phone(std::move(tel)) {}
 
 void Teacher::display() const {
     cout << name << ' ' << age << ' ' << sex << ' '
          << title << ' ' << address << ' ' << phone;
 }
 // class Cadre
-Cadre::Cadre(string n, string a, string s, string p, string addr, string tel)
-    : name(std::move(n)), age(std::move(a)), sex(std::move(s)), post(std::move(p)), address(std::move(addr)), phone(std::move(tel)) {}
+Cadre::Cadre(string n, string a, string s, 
+    string p, string addr, string tel)
+    : name(std::move(n)), age(std::move(a)), 
+    sex(std::move(s)), post(std::move(p)), 
+    address(std::move(addr)), phone(std::move(tel)) {}
 // class Teacher_Cadre
-Teacher_Cadre::Teacher_Cadre(string n, string a, string s, string t, string p, string addr, string tel, double w)
-    : Teacher(n, a, s, t, addr, tel), Cadre(n, a, s, p, addr, tel), wages(w) {}
+Teacher_Cadre::Teacher_Cadre(string n, string a, string s, 
+    string t, string p, string addr, string tel, double w)
+    : Teacher(n, a, s, t, addr, tel), 
+    Cadre(n, a, s, p, addr, tel), wages(w) {}
 
 void Teacher_Cadre::show() const {
     Teacher::display();
@@ -53,8 +65,12 @@ void Teacher_Cadre::show() const {
 int main() {
     string name, age, sex, title, addr, phone, post;
     double wages;
-    cin >> name >> age >> sex >> title >> addr >> phone >> post >> wages;
+    cin >> name >> age >> sex >> title 
+        >> addr >> phone >> post >> wages;
 
-    Teacher_Cadre ng(name, age, sex, title, post, addr, phone, wages);
+    Teacher_Cadre ng(name, age, sex, 
+        title, post, addr, phone, wages);
     ng.show();
+
+    cout<<"田佩宁 202512898\n";
 }
